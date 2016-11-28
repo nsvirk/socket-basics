022a5fa4226d771750fac85a18df001e5df48f6e
nsvirk <nsvirk@gmail.com>
2016-11-29 00:46:01 +0530

    Add name support with query params

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ io.on('connection', function(socket){
 		io.emit('message', message);
 	});
 
-	socket.emit('message', {		
+	socket.emit('message', {	
+		name: 'System',	
 		text: 'Welcome to the chat application!',
 		timestamp : moment().valueOf()
 	});
