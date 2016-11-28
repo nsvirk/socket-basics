ad322d75143aae771bd63c626465bcfe00f407eb
nsv <nsv@MacPro61.local>
2016-11-28 22:33:09 +0530

    Add frontend submit to submit message

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/*//==================================
+var KiteTicker = require("kiteconnect").KiteTicker;
+var ticker = new KiteTicker("kitefront", "SA0846", "1e2b551ef9e514b09efe768354c3f464");
+*///==================================
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
@@ -19,9 +24,32 @@ io.on('connection', function(socket){
 		text: 'Welcome to the chat application!'
 	});
 
+	/*/==================================
+	ticker.connect();
+	ticker.on("tick", setTick);
+	ticker.on("connect", subscribe);
+
+	function setTick(ticks) {
+	    //console.log("Ticks", ticks);
+	    socket.emit('message', {
+		text: ticks
+		});
+	}
+
+	function subscribe() {
+	    var items = [256265, 53357575, 53325575, 53297415, 53315591];
+	    ticker.subscribe(items);
+	    ticker.setMode(ticker.modeFull, items);
+	}
+	*///==================================
+
 });
 
 
 http.listen(PORT, function(){
 	console.log('Server started!');
 });
+
+
+
+
