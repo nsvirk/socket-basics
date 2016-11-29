d68b937d5844bb4a5ac8b22806254d2e1f709b39
nsvirk <nsvirk@gmail.com>
2016-11-29 09:34:31 +0530

    Add @currentUsers command

--- server.js
+++ server.js
@@ -11,10 +11,48 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach( function(socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room){
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp : moment().valueOf()	
+	});
+}
+
+
+
 io.on('connection', function(socket){
 	
 	console.log('User connected via socket.io');
 
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left',
+				timestamp : moment().valueOf()				
+			});
+			delete clientInfo[socket.id];
+		}
+	});
 
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
@@ -29,9 +67,12 @@ io.on('connection', function(socket){
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = moment().valueOf();
-
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);			
+		}
 	});
 
 	socket.emit('message', {	
